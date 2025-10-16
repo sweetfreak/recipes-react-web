e9377e70e246989ea0a0adb6b13233667b5125b3
jesse sheehan <jessetsheehan@gmail.com>
2025-10-16 12:04:28 -0400

    hiding stuff

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCFpF-V3mMU3-H_fs-a3m7HIoMNTLRhKWI",
-  authDomain: "recipe-react-app-a8ca5.firebaseapp.com",
-  projectId: "recipe-react-app-a8ca5",
-  storageBucket: "recipe-react-app-a8ca5.firebasestorage.app",
-  messagingSenderId: "440492389417",
-  appId: "1:440492389417:web:7bf8eef3096858a1be5838"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
